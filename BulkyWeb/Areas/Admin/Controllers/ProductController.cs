cc1ffc008191c275c6957d59464bb3e231089708
C.YOGESH <chintalayogesh77@gmail.com>
2025-12-17 18:20:38 +0530

    Implement ViewModel

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Areas.Admin.Controllers
@@ -18,26 +20,56 @@ namespace BulkyWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
+            //    .GetAll().Select(u => new SelectListItem
+            //    {
+            //        Text = u.Name,
+            //        Value = u.Id.ToString()
+            //    });
             return View(objProductList);
         }
         public IActionResult Create()
         {
-            return View();
+            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category SelectListItem is a class in .NET (specifically within the Microsoft.AspNetCore.Mvc.Rendering or System.Web.Mvc namespaces) used to represent a single item within an HTML <select> (dropdown or list box) element
+            //   .GetAll().Select(u => new SelectListItem
+            //   {
+            //       Text = u.Name,
+            //       Value = u.Id.ToString() 
+            //   });
+            //ViewBag.CategoryList = CategoryList;
+            //ViewData["CategoryList"] = CategoryList; // before using ViewModel
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+               {
+                   Text = u.Name,
+                   Value = u.Id.ToString()
+               }),
+                Product = new Product()
+            };
+            return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
            
             if (ModelState.IsValid)
             {
                 //_db.Categories.Add(obj);
                 //_db.SaveChanges();
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                  return RedirectToAction("Index");
             }
-            return View();
+            else {//Re-populate CategoryList in case of validation errors
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+            return View(productVM);
+            }
         }
         public IActionResult Edit(int? id)
         {
